f3e0889be351dc9593913ab4d7cf75e238c1dd09
Edan Moz <blagatass@gmail.com>
2021-03-14 00:37:01 -0300

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,26 @@
+#General Outline for project
+
+### Set-up for working on the file.
+#First open source video file
+#Create an output working folder.
+#Output videos to 
+
+### First Step: Process the resulting images.
+#Start at the first image file.
+#Check every picture in pairs.
+#If the pictures are the same, delete the first one.
+#Repeat until all redundant images are deleted.
+
+### Second Step: (Optional) Delete parts of the image which repeat on the same place.
+#Start at the first image file.
+#Compare pictures in pairs.
+#Every part of the images that is identical on the same place gets deleted on the first picture.
+#When reached the end, get the last 2 files and delete repeating in both files.
+
+### Third Step: Process the images to get the final image.
+#Compare pictures in pairs.
+#Add 2 pictures where they fit.
+#Output compound image.
+#Delete the original 2 pictures.
+
+#Finished.
